c01376c6362b049d808f49dc326c49008b01ac94
Robert J Martin <robertmartinbc@shaw.ca>
2013-08-15 10:11:11 -0700

    First Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FancyboxTest::Application.config.secret_token = 'f83c6cca445b7ef52e643aabb64fcacb36adf4369f0d585c66aa8f89204e3ce6fb3104d37619c51b1a441ad95f23907a4f7ff7cc367684ed2a7cdc6522e5cfab'
